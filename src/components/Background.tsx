e4ac8be7bc3bcc7f2322a19e919e6f8302e4119f
mxclfld <crackfield777@gmail.com>
2023-12-03 18:22:13 +0200

    feat: add mobile adaptation

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -5,8 +5,8 @@ import { Typewriter } from "react-simple-typewriter";
 
 export default function Background() {
   return (
-    <section className="flex flex-col justify-center min-h-screen lg:gap-4 w-full">
-      <div className='blob -bottom-1/3 right-1/3 absolute' />
+    <section className="mt-40 sm:mt-0 flex flex-col justify-center min-h-screen lg:gap-4 w-full">
+      <div className='blob -bottom:3/4 sm:-bottom-1/3 right-1/3 absolute' />
       <div className="flex flex-col flex-initial gap-12 lg:gap-8 xl:gap-12 2xl:gap-20 lg:flex-row lg:justify-between lg:items-center">
         <div className="w-full lg:w-1/4 self-start lg:text-end font-bold text-xl bg-primary text-black px-2 mix-blend-difference">
           <Typewriter words={["Background"]} loop cursor cursorStyle="_" delaySpeed={4000} typeSpeed={400} />
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -10,14 +10,14 @@ import Skills from './Skills';
 export default function HeroSection() {
   return (
     <section className="flex flex-col justify-center min-h-[95vh] lg:gap-4 w-full relative">
-      <div className="flex flex-col flex-initial gap-12 lg:gap-8 xl:gap-12 2xl:gap-20 lg:flex-row lg:justify-between lg:items-center">
+      <div className="flex flex-col flex-initial gap-36 lg:gap-8 xl:gap-12 2xl:gap-20 lg:flex-row lg:justify-between lg:items-center">
         <Introduction />
         <Skills />
       </div>
       <motion.div
         animate={{ bottom: [0, 50, 0] }}
         transition={{ repeat: Infinity, repeatDelay: 0.7 }}
-        className="absolute right-1/2"
+        className="hidden lg:block absolute right-1/2"
       >
         <FaArrowDownLong />
       </motion.div>
